a31df8a71260f95e96d1f73929b9939ac2d63d62
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-05-27 11:18:06 -0500

    creando juego con herencias

---
+++ game/src/main/java/com/mycompany/game/Game.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.game;
+
+
+public class Game {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ game/src/main/java/com/mycompany/game/spaceship/NuclearShip.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.game.spaceship;
+
+
+public class NuclearShip {
+    private String name;
+    private boolean armed;
+    private int crew;
+    private int damage;
+    private int health;
+    private int speed;
+    
+    public void fly(){
+        System.out.println("¡A volar!");
+    }
+    
+    public void shoot(){
+        
+    }
+}
